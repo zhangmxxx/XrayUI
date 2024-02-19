d97b90e824e583286c1e96bf312044a894daf2c3
Zhangmxxx <211240005@smail.nju.edu.cn>
2024-02-19 11:42:17 +0800

    Initial Commit

---
+++ Xray-UI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Xray-UI
+//
+//  Created by Simenons 张 on 2024/2/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Xray-UI/Xray_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xray_UIApp.swift
+//  Xray-UI
+//
+//  Created by Simenons 张 on 2024/2/19.
+//
+
+import SwiftUI
+
+@main
+struct Xray_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
